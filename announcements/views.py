518e8db77b23c7eca6645a24a179c67893f03366
Ilya Stritskiy <msv_ilya@mail.ru>
2024-05-14 16:21:57 +0300

    was added 2 views: list of announcement, creating announcement

--- announcements/views.py
+++ announcements/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView
 
-# Create your views here.
+from .models import Announcement
+from .forms import AnnouncementForm
+
+
+class AnnouncementList(ListView):
+    model = Announcement
+    ordering = '-time_in'
+    template_name = 'announcements/list.html'
+    context_object_name = 'announcements'
+    paginate_by = 5
+
+
+class AnnouncementCreate(CreateView):
+    form_class = AnnouncementForm
+    model = Announcement
+    template_name = 'announcements/create.html'
