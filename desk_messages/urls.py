374a83726fdc6ccec18c17e780f2822e4f2c959e
Ilya Stritskiy <msv_ilya@mail.ru>
2024-05-14 14:59:06 +0300

    was added URL for ckeditor extension

--- desk_messages/urls.py
+++ desk_messages/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
+    # path to admin panel
     path('admin/', admin.site.urls),
+
+    # paths for extensions apps
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
