e83a2de1ee01d17e77e4cfaf616e92a814d5a8eb
Ilya Stritskiy <msv_ilya@mail.ru>
2024-05-14 18:43:46 +0300

    was added new paths for detail, create, update views

--- announcements/urls.py
+++ announcements/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import AnnouncementList, AnnouncementCreate
+from .views import AnnouncementList, AnnouncementDetail, AnnouncementCreate, AnnouncementUpdate
 
 
 urlpatterns = [
     path('', AnnouncementList.as_view(), name='announcement_list'),
-    path('create/', AnnouncementCreate.as_view(), name='announcement_create')
+    path('<int:pk>', AnnouncementDetail.as_view(), 'announcemet_detail'),
+    path('create/', AnnouncementCreate.as_view(), name='announcement_create'),
+    path('update/', AnnouncementUpdate.as_view(), name='announcement_update'),
 ]
