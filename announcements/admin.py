55a75d6201d9aee42f378484e6837d63bc93c798
Ilya Stritskiy <msv_ilya@mail.ru>
2024-05-14 14:59:57 +0300

    was registered all models of announcements app

--- announcements/admin.py
+++ announcements/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django import forms
 
-# Register your models here.
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+from .models import Announcement, Author, Category, Comment
+
+
+class AnnouncementAdminForm(forms.ModelForm):
+    announcement_text = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Announcement
+        fields = '__all__'
+
+
+class AnnouncementAdmin(admin.ModelAdmin):
+    """ Announcements """
+    form = AnnouncementAdminForm
+
+
+admin.site.register(Announcement, AnnouncementAdmin)
+admin.site.register(Author)
+admin.site.register(Category)
+admin.site.register(Comment)
