0b54978960aaa7206cac28062f696bfb8286ec6d
Ilya Stritskiy <msv_ilya@mail.ru>
2024-05-16 01:05:37 +0300

    was changed 1 field: announcement_text

--- announcements/models.py
+++ announcements/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+from ckeditor_uploader.fields import RichTextUploadingField
+
 
 class Author(models.Model):
     author_name = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -47,7 +49,7 @@ class Category(models.Model):
 class Announcement(models.Model):
     time_in = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=115)
-    announcement_text = models.TextField()
+    announcement_text = RichTextUploadingField()
 
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='announcements')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='announcements')
