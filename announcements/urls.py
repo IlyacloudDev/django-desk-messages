7cba5b2df46b5dd73bf89d0289e0a71c49842f65
Ilya Stritskiy <msv_ilya@mail.ru>
2024-05-18 02:19:06 +0300

    was fixed mistake with name view in path

--- announcements/urls.py
+++ announcements/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import AnnouncementList, AnnouncementDetail, AnnouncementCreate, AnnouncementUpdate, AuthorAnnounecemntList
+from .views import AnnouncementList, AnnouncementDetail, AnnouncementCreate, AnnouncementUpdate, AuthorAnnouncementList
 
 
 urlpatterns = [
@@ -7,5 +7,5 @@ urlpatterns = [
     path('<int:pk>/', AnnouncementDetail.as_view(), name='announcement_detail'),
     path('create/', AnnouncementCreate.as_view(), name='announcement_create'),
     path('update/<int:pk>/', AnnouncementUpdate.as_view(), name='announcement_update'),
-    path('own/', AuthorAnnounecemntList.as_view(), name='author_announcements'),
+    path('own/', AuthorAnnouncementList.as_view(), name='author_announcements'),
 ]
