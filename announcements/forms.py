49e573470ec6aa82c22f0715b5d51db3ec4d4903
Ilya Stritskiy <msv_ilya@mail.ru>
2024-05-18 17:01:43 +0300

    was deleted field 'author' from AnnouncementForm

--- announcements/forms.py
+++ announcements/forms.py
@@ -1,7 +1,4 @@
 from django import forms
-from django.core.exceptions import ValidationError
-
-from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
 from .models import Announcement
 
@@ -9,4 +6,4 @@ from .models import Announcement
 class AnnouncementForm(forms.ModelForm):
     class Meta:
         model = Announcement
-        fields = ['title', 'author', 'category', 'announcement_text']
+        fields = ['title', 'category', 'announcement_text']
