90bf6a3fb680671b52e2b1ec774ff19e22bf8610
Ilya Stritskiy <msv_ilya@mail.ru>
2024-05-18 23:44:27 +0300

    signals is inactive on time

--- announcements/signals.py
+++ announcements/signals.py
@@ -1,10 +1,1 @@
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-from .models import Author
-
-
-@receiver(post_save, sender=User)
-def user_created(instance, **kwargs):
-    author = Author.objects.create(author_name=instance.id)
+# while this file is inactive
