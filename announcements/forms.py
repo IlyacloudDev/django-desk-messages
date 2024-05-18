c049336616cb5213f4d1db0cecc593c19d8d1f24
Ilya Stritskiy <msv_ilya@mail.ru>
2024-05-18 23:43:25 +0300

    was added CustomSignupForm: create new author from new registered user

--- announcements/forms.py
+++ announcements/forms.py
@@ -1,9 +1,18 @@
 from django import forms
 
-from .models import Announcement
+from allauth.account.forms import SignupForm
+
+from .models import Announcement, Author
 
 
 class AnnouncementForm(forms.ModelForm):
     class Meta:
         model = Announcement
         fields = ['title', 'category', 'announcement_text']
+
+
+class CustomSignupForm(SignupForm):
+    def save(self, request):
+        user = super(CustomSignupForm, self).save(request)
+        Author.objects.create(author_name=user)
+        return user
