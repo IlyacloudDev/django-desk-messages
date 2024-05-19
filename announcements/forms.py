0697ca8cbb5bdf23860df2fcf55812b8997a9f92
Ilya Stritskiy <msv_ilya@mail.ru>
2024-05-20 00:32:58 +0300

    was added new form: CommentForm - form to create comment

--- announcements/forms.py
+++ announcements/forms.py
@@ -2,13 +2,19 @@ from django import forms
 
 from allauth.account.forms import SignupForm
 
-from .models import Announcement, Author
+from .models import Announcement, Author, Comment
 
 
 class AnnouncementForm(forms.ModelForm):
     class Meta:
         model = Announcement
         fields = ['title', 'category', 'announcement_text']
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['comment_text']
 
 
 class CustomSignupForm(SignupForm):
