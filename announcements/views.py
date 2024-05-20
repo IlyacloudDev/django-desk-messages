18e72fa63cd95ade808799fb34254314d565b128
Ilya Stritskiy <msv_ilya@mail.ru>
2024-05-20 19:16:24 +0300

    was added 2 new views for: deleting, allowing comment; was added LoginRequiredMixin for checking ability to create comment

--- announcements/views.py
+++ announcements/views.py
@@ -1,5 +1,8 @@
-from django.views.generic import View, ListView, CreateView, UpdateView, DetailView
+from django.urls import reverse_lazy
+from django.views.decorators.csrf import csrf_protect
+from django.views.generic import View, ListView, CreateView, UpdateView, DetailView, DeleteView
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Announcement, Author, Comment
 from .forms import AnnouncementForm, CommentForm
@@ -20,7 +23,7 @@ class AnnouncementDetail(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
-        context['comments'] = Comment.objects.filter(announcement=self.kwargs['pk'])
+        context['comments'] = Comment.objects.filter(announcement=self.kwargs['pk'], allowed=True)
         return context
 
 
@@ -55,7 +58,7 @@ class AuthorAnnouncementList(View):
         return render(request, 'announcements/announcements_of_user.html', context={'announcements': announcements})
 
 
-class CommentCreate(CreateView):
+class CommentCreate(LoginRequiredMixin, CreateView):
     form_class = CommentForm
     model = Comment
     template_name = 'comments/create.html'
@@ -66,3 +69,18 @@ class CommentCreate(CreateView):
         form.instance.announcement = Announcement.objects.get(pk=self.kwargs['pk'])
         comment.save()
         return super().form_valid(form)
+
+
+class CommentDelete(DeleteView):
+    model = Comment
+    template_name = 'comments/delete.html'
+    success_url = reverse_lazy('author_announcements')
+
+
+class CommentAllow(View):
+    def post(self, request, **kwargs):
+        action = request.POST.get('action')
+        comment = Comment.objects.filter(pk=self.kwargs['pk'])
+        if action == 'allow':
+            Comment.objects.filter(pk=self.kwargs['pk']).update(allowed=True)
+        return render(request, 'comments/allow.html', context={'comment': comment})
