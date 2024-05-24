6de426839310e17a1863c1287034afd847dce990
Ilya Stritskiy <msv_ilya@mail.ru>
2024-05-24 22:13:40 +0300

    was added configuration for period tasks

--- desk_messages/celery.py
+++ desk_messages/celery.py
@@ -1,6 +1,7 @@
 import os
-from celery import Celery
 
+from celery import Celery
+from celery.schedules import crontab
 
 # связываем настройки Django с настройками Celery через меременную окружения
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'desk_messages.settings')
@@ -13,3 +14,10 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # указываем Celery автоматически искать задания в файлах tasks.py каждого приложения проекта
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'sending_email_every_monday_0am': {
+        'task': 'announcements.tasks.weekly_notification',
+        'schedule': crontab()
+    }
+}
