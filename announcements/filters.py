2b07d1b5154f7c3e8b4c6825a1dde6ad3f811ac9
Ilya Stritskiy <msv_ilya@mail.ru>
2024-05-26 04:39:01 +0300

    was added new filter: CommentFilter - filtering list of comments for selected announcement

--- announcements/filters.py
+++ announcements/filters.py
@@ -1,6 +1,6 @@
 from django_filters import FilterSet
 
-from .models import Announcement
+from .models import Announcement, Comment
 
 
 class AnnouncementsFilter(FilterSet):
@@ -9,3 +9,15 @@ class AnnouncementsFilter(FilterSet):
         fields = {
             'title': ['icontains'],
         }
+
+
+class CommentFilter(FilterSet):
+    class Meta:
+        model = Comment
+        fields = [
+            'announcement'
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super(CommentFilter, self).__init__(*args, **kwargs)
+        self.filters['announcement'].queryset = Announcement.objects.filter(author__author_name=kwargs['request'])
