969f0ce224d5539c1a4129973b0256cf80cf735e
Ilya Stritskiy <msv_ilya@mail.ru>
2024-05-26 18:02:35 +0300

    was added mechanism of creating single code for verification of email

--- announcements/forms.py
+++ announcements/forms.py
@@ -1,4 +1,10 @@
+import random
+
+from string import hexdigits
+
 from django import forms
+from django.conf import settings
+from django.core.mail import send_mail
 
 from allauth.account.forms import SignupForm
 
@@ -21,4 +27,14 @@ class CustomSignupForm(SignupForm):
     def save(self, request):
         user = super(CustomSignupForm, self).save(request)
         Author.objects.create(author_name=user)
+        user.is_active = False
+        code = ''.join(random.sample(hexdigits, 5))
+        user.code = code
+        user.save()
+        send_mail(
+            subject=f'Код активации',
+            message=f'Код активации аккаунта: {code}',
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[user.email],
+        )
         return user
